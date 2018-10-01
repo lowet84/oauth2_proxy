100e68a8e7e1bfda34ad5fff83e6a21977c29292
root <root@Ubuntu.asdu3tqgoxnuzcsg1dc3czbidb.fx.internal.cloudapp.net>
2018-10-01 08:36:47 +0000

    --help

--- docker/start.sh
+++ docker/start.sh
@@ -1,1 +1,6 @@
-/bin/oauth2_proxy   --cookie-secure=false  --upstream="http://$SERVICE"  --http-address="0.0.0.0:4180" --provider=gitlab --login-url="https://gitlab.$SERVER_DOMAIN/oauth/authorize" --redeem-url="http://gitlab/oauth/token" --validate-url="http://gitlab/api/v4/user" --redirect-url="https://portal.$SERVER_DOMAIN/oauth2/callback" --gitlab-group=$GROUP  --email-domain=* --scope="api read_user" --client-id $CLIENT_ID --cookie-secret $COOKIE_SECRET --client-secret $CLIENT_SECRET
+#echo "server domain: $SERVER_DOMAIN"
+#echo "cookie secret: $COOKIE_SECRET"
+#echo "client id: $CLIENT_ID"
+#echo "client secret: $CLIENT_SECRET"
+
+/bin/oauth2_proxy   --cookie-secure=false  --upstream="http://$SERVICE"  --http-address="0.0.0.0:4180" --provider=gitlab --login-url="https://gitlab.$SERVER_DOMAIN/oauth/authorize" --redeem-url="http://gitlab/oauth/token" --validate-url="http://gitlab/api/v4/user" --redirect-url="https://portal.$SERVER_DOMAIN/oauth2/callback" --gitlab-group=$GROUP  --email-domain=* --scope="api read_user" --client-id "$CLIENT_ID" --cookie-secret "$COOKIE_SECRET" --client-secret "$CLIENT_SECRET" --cookie-domain "$SERVER_DOMAIN"
